8cb51254754d6176de7115f8f8003427bdd060b0
GaetanRAYNAUD <gaetanraynaud@outlook.com>
2020-12-13 21:10:25 +0100

    fix: game options

--- src/main/java/fr/osallek/eu4parser/model/save/gameplayoptions/GameplayOptions.java
+++ src/main/java/fr/osallek/eu4parser/model/save/gameplayoptions/GameplayOptions.java
@@ -175,7 +175,7 @@ public class GameplayOptions {
     }
 
     public void setOnlyHostCanPause(boolean onlyHostCanPause) {
-        this.list.set(23, onlyHostCanPause ? 1 : 0);
+        this.list.set(26, onlyHostCanPause ? 1 : 0);
     }
 
     public boolean getShowLoadingScreen() {
