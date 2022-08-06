c016b93163cadf2fdfb34efa7289155cce89bb9a
GaetanRAYNAUD <gaetanraynaud@outlook.com>
2022-08-06 17:23:20 +0200

    Zip linux path

--- src/main/java/fr/osallek/eu4parser/common/ZipUtils.java
+++ src/main/java/fr/osallek/eu4parser/common/ZipUtils.java
@@ -1,21 +1,22 @@
 package fr.osallek.eu4parser.common;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.function.Predicate;
-import java.util.zip.ZipOutputStream;
-import org.apache.commons.io.FileUtils;
-
-import java.io.IOException;
-import java.nio.file.Path;
 import java.util.Enumeration;
+import java.util.function.Predicate;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
-import org.apache.commons.io.IOUtils;
+import java.util.zip.ZipOutputStream;
 
 public final class ZipUtils {
 
@@ -23,26 +24,24 @@ public final class ZipUtils {
     }
 
     public static void zipFolder(Path sourceFolderPath, Path zipPath, Predicate<Path> predicate) throws IOException {
-        ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipPath.toFile()));
+        try (ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipPath.toFile()))) {
+            Files.walkFileTree(sourceFolderPath, new SimpleFileVisitor<>() {
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    if (!predicate.test(file)) {
+                        return FileVisitResult.CONTINUE;
+                    }
 
-        Files.walkFileTree(sourceFolderPath, new SimpleFileVisitor<>() {
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                if (!predicate.test(file)) {
+                    zos.putNextEntry(new ZipEntry(FilenameUtils.separatorsToUnix(sourceFolderPath.relativize(file).toString())));
+
+                    try (InputStream stream = Files.newInputStream(file)) {
+                        IOUtils.copy(stream, zos, 1_000_000);
+                    }
+
+                    zos.closeEntry();
                     return FileVisitResult.CONTINUE;
                 }
-
-                zos.putNextEntry(new ZipEntry(sourceFolderPath.relativize(file).toString()));
-
-                try (InputStream stream = Files.newInputStream(file)) {
-                    IOUtils.copy(stream, zos, 1_000_000);
-                }
-
-                zos.closeEntry();
-                return FileVisitResult.CONTINUE;
-            }
-        });
-
-        zos.close();
+            });
+        }
     }
 
     public static Path newFile(Path destinationDir, ZipEntry zipEntry) throws IOException {
